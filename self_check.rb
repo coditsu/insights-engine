6b12c45a2627d75c74faa5c929a2039386291d32
Maciej Mensfeld <maciej@mensfeld.pl>
2016-11-02 19:04:06 +0100

    in

--- self_check.rb
+++ self_check.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 require './lib/insights_engine.rb'
 
-build_path = '/home/mencio/Software/Connective/automater'
+build_path = '/home/mencio/Software/Coditsu/Kabe'
 
-p InsightsEngine::Harvesters::TrackedFiles::Engine.new.call(
+p InsightsEngine::Harvesters::Linguist::Engine.new.call(
   build_path: build_path
 )
