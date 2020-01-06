1680634d80c38cef723fe2f0876d7e8afa8b1ddd
Maciej Mensfeld <maciej@mensfeld.pl>
2020-01-06 13:04:58 +0100

    order before require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,7 +25,7 @@ end
 
 Time.zone = 'UTC'
 
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
